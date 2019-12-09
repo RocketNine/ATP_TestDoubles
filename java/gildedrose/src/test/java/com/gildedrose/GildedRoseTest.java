ca26002354f5960468f7e1d7e9f513ffaf6e97ef
Paul Moore <pdmoore@gmail.com>
2019-12-08 16:04:42 -0800

    Example - Gilded Rose requires Dummy for item name

--- java/gildedrose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ java/gildedrose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,16 +6,23 @@ import static org.junit.Assert.assertEquals;
 
 public class GildedRoseTest {
 
+    private final String IGNORED_STRING = null;
+    private final int IGNORED_INT = -99;
+
+    /*
+      This test will fail if the item name is null.
+      The behavior does not interact with the item name but requires a non-null, non-special name.
+     */
     @Test
-    public void this_test_needs_a_better_name() {
+    public void SellIn_Decreases_GenericItem() {
         // Arrange
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[] { new Item(IGNORED_STRING, 10, IGNORED_INT) };
         GildedRose sut = new GildedRose(items);
 
         // Act
         sut.updateQuality();
 
         // Assert
-        assertEquals("fixme", sut.items[0].name);
+        assertEquals(9, sut.items[0].sellIn);
     }
 }
