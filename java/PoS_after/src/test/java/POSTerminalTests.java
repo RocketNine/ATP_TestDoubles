4e703e01d0810663541ab5aedb47bbb81d2b596d
Paul Moore <pdmoore@gmail.com>
2025-12-14 12:52:27 -0800

    clear up problems reported by sonar

--- java/PoS_after/src/test/java/POSTerminalTests.java
+++ java/PoS_after/src/test/java/POSTerminalTests.java
@@ -3,36 +3,36 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
-public class POSTerminalTests {
+class POSTerminalTests {
 
     @Test
-    public void VerifySale_AmountUnderSingleChargeLimit_IsApproved() {
+    void VerifySale_AmountUnderSingleChargeLimit_IsApproved() {
         // Using the real instance is costly in terms of time
 
         POSTerminal sut = new POSTerminal();
 
         try {
             sut.verifySale(10);
-        } catch (POSTerminal.InvalidChargeException e) {
+        } catch (POSTerminal.InvalidChargeException _) {
             fail("Amount was under Single Charge Amount - exception not expected!");
         }
     }
 
     @Test
     @Disabled("Remove to show faster feedback by using Test Double")
-    public void VerifySale_ViaMock_AmountUnderSingleChargeLimit_IsApproved() {
+    void VerifySale_ViaMock_AmountUnderSingleChargeLimit_IsApproved() {
         ICCVerifier mockCCVerifier = new MockCCVerifier();
 
         POSTerminal sut = new POSTerminal(mockCCVerifier);
 
         try {
             sut.verifySale(10);
-        } catch (POSTerminal.InvalidChargeException e) {
+        } catch (POSTerminal.InvalidChargeException _) {
             fail("Amount was under Single Charge Amount - exception not expected!");
         }
     }
 
-    class MockCCVerifier implements ICCVerifier {
+    static class MockCCVerifier implements ICCVerifier {
 
         @Override
         public boolean approveCharge(double amount) {
